255a4f324d6e1b6cfa151b77c43caa0d7edfc39a
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-16 02:31:49 +0500

    fix

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("Message sent: %s", "ok ha ye to"),
+		Body:       fmt.Sprintf("Message sent: %s", "ok ha ye to jani"),
 	}
 	return response, nil
 }

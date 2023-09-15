cef09abb9fcd60b3c0714a8eb880b6949c747303
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-16 04:59:39 +0500

    fix

--- main.go
+++ main.go
@@ -56,9 +56,34 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	log.Printf("Message sent to the queue: %s", message)
 
+	// Subscribe to a queue on the broker
+	sub, err := conn.Subscribe(queueName, stomp.AckAuto)
+	if err != nil {
+		log.Printf("Failed to subscribe to the queue: %v", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+	defer sub.Unsubscribe()
+
+	fmt.Print("Connection established, waiting for messages...\n")
+
+	// Listen for and process incoming messages
+	var messageBody string
+	for {
+		msg := <-sub.C
+		if msg.Err != nil {
+			log.Printf("Failed to receive message: %v", msg.Err)
+			return events.APIGatewayProxyResponse{StatusCode: 500}, msg.Err
+		}
+
+		// Process the received message (you can modify this part as needed)
+		messageBody = string(msg.Body)
+		log.Printf("Received message from the queue: %s", messageBody)
+		break
+	}
+
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("Message sent: %s", "ok"),
+		Body:       fmt.Sprintf("Message sent: %s and recieved also %s", "done", messageBody),
 	}
 	return response, nil
 }

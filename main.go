8ddc0d453390747a905600c3d49ab45fd1b07c8e
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-09-18 15:18:09 +0500

    fix

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	// Send a message to a queue on the broker
 	queueName := "Demo-Queue"
-	message := "Hello, Amazon MQ"
+	message := request.Body
 	err = conn.Send(
 		queueName,
 		"text/plain",
